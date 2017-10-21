594c7c3b2891a6daa59b8176a2e15c7b4e827d0b
Corey Cole <coreyleoc@gmail.com>
2017-10-20 23:27:56 -0700

    refactor: organize components into directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { IntroCardComponent } from './intro-card/intro-card.component';
-import { ProjectGalleryComponent } from './project-gallery/project-gallery.component';
-import { ProjectPreviewCardComponent } from './project-preview-card/project-preview-card.component';
+import { IntroCardComponent } from './components/intro-card/intro-card.component';
+import { ProjectGalleryComponent } from './components/project-gallery/project-gallery.component';
+import { ProjectPreviewCardComponent } from './components/project-preview-card/project-preview-card.component';
 import { ProjectPageComponent } from './pages/project-page/project-page.component';
-import { ProjectHeaderComponent } from './project-header/project-header.component';
-import { ProjectStorylineComponent } from './project-storyline/project-storyline.component';
-import { ProjectStorylineComponentComponent } from './project-storyline-component/project-storyline-component.component';
+import { ProjectHeaderComponent } from './components/project-header/project-header.component';
+import { ProjectStorylineComponent } from './components/project-storyline/project-storyline.component';
+import { ProjectStorylineComponentComponent } from './components/project-storyline-component/project-storyline-component.component';
 import { EditProjectStorylinePageComponent } from './pages/edit-project-storyline-page/edit-project-storyline-page.component';
 
 @NgModule({
