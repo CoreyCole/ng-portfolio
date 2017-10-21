89c7784d291ea0124604f13e33cf5423c56279da
Corey Cole <coreyleoc@gmail.com>
2017-10-20 23:26:33 -0700

    refactor: organize code, change sign up to page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { IntroCardComponent } from './intro-card/intro-card.component';
 import { ProjectGalleryComponent } from './project-gallery/project-gallery.component';
 import { ProjectPreviewCardComponent } from './project-preview-card/project-preview-card.component';
-import { ProjectPageComponent } from './project-page/project-page.component';
+import { ProjectPageComponent } from './pages/project-page/project-page.component';
 import { ProjectHeaderComponent } from './project-header/project-header.component';
 import { ProjectStorylineComponent } from './project-storyline/project-storyline.component';
 import { ProjectStorylineComponentComponent } from './project-storyline-component/project-storyline-component.component';
-import { EditProjectStorylinePageComponent } from './edit-project-storyline-page/edit-project-storyline-page.component';
+import { EditProjectStorylinePageComponent } from './pages/edit-project-storyline-page/edit-project-storyline-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SignUpComponent,
+    SignUpPageComponent,
     HomePageComponent,
     IntroCardComponent,
     ProjectGalleryComponent,
---
+++ src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-page',
+  template: `
+    <p>
+      sign-up-page works!
+    </p>
+  `,
+  styleUrls: ['./sign-up-page.component.scss']
+})
+export class SignUpPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
