e63d6b90c9dcdb52bb4ae51cdae6cf6f7a2d9933
Corey Cole <coreyleoc@gmail.com>
2017-10-21 21:12:47 -0700

    feat: homepage html

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Project } from '../../../models/project';
 
 @Component({
   selector: 'app-home-page',
+  styleUrls: ['./home-page.component.scss'],
   template: `
-    <p>
-      home-page works!
-    </p>
-  `,
-  styleUrls: ['./home-page.component.scss']
+    <div class="header">
+      <app-intro-card></app-intro-card>
+      <div class="spacing"></div>
+    </div>
+    <app-project-gallery [projects]="projects | async"></app-project-gallery>
+  `
 })
 export class HomePageComponent implements OnInit {
+
+  public projects: Observable<Project[]>;
 
   constructor() { }
 
