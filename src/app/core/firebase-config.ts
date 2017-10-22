2448dc89751ac4559c3bc85a2ef85e21ed6e646b
Corey Cole <coreyleoc@gmail.com>
2017-10-22 02:08:19 -0700

    fix: dont track firebase-config

--- src/app/core/firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAzEFA2eEV6a7oXjWxrU37VKHHUsbswNXc',
-  authDomain: 'ng-portfolio-dubhacks.firebaseapp.com',
-  databaseURL: 'https://ng-portfolio-dubhacks.firebaseio.com',
-  projectId: 'ng-portfolio-dubhacks',
-  storageBucket: 'ng-portfolio-dubhacks.appspot.com',
-  messagingSenderId: '730143000895'
-};
