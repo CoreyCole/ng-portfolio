700e1dac4e557888e57faa329b4cb886f65452c1
Corey Cole <coreyleoc@gmail.com>
2017-10-22 01:34:37 -0700

    feat: scroll toolbar

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import './polyfills';
+import 'zone.js';
+import 'reflect-metadata';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
